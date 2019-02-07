d5e2f48f26f8715759ac0b6040047da4636625a8
Winnie <wwk773@gmail.com>
2019-02-07 10:58:10 +0630

    Add project files.

---
+++ MobileApp4/MobileApp4/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileApp4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void NavigateButton_OnClicked(object sender,EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
---
+++ MobileApp4/MobileApp4/SecondPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp4
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SecondPage : ContentPage
+	{
+		public SecondPage ()
+		{
+			InitializeComponent ();
+		}
+        private async void NavigateButton_OnClicked(object sender,EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+	}
+}
